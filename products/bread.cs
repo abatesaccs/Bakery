d202315998c430959ac1c01c711fbb7f0eb26444
Andrew <abatesaccs@gmail.com>
2019-12-13 12:53:25 -0800

    create bread constructor and get/set for loaves

--- products/bread.cs
+++ products/bread.cs
@@ -4,6 +4,20 @@ namespace products.bread
 {
   public class Bread
   {
+    private int _Loaves;
+    private int _Price;
+    public Bread(int loaves, int price)
+    {
+      _Loaves = loaves;
+      _Price = price;
+    }
+    public int getLoaves()
+    {
+      return _Loaves;
+    }
+    public int buyLoaves(int loaves)
+    {
 
+    }
   }
 }
